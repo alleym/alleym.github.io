4f571ae27ff331474062bbae5c7470a43b5931e2
Michelle Alley Account <shecalledme@gmail.com>
2019-06-03 11:56:45 -0500

    more changes time photos lesson 6

--- lesson-6/javascript/currentdate.js
+++ lesson-6/javascript/currentdate.js
@@ -1,2 +1,4 @@
-var d = new Date();
-document.getElementById("demo").innerHTML = d;
+document.write ('<p>Current time is: <span id="date-time">', new Date().toLocaleString(), '<\/span>.<\/p>')
+if (document.getElementById) onload = function () {
+  setInterval ("document.getElementById ('date-time').firstChild.data = new Date().toLocaleString()", 50)
+}
