5fa0ad91240493d169e1d140fabcb671377c4384
Michelle Alley Account <shecalledme@gmail.com>
2019-06-29 20:27:06 -0500

    updates

--- lesson-10/javascript/weatherAppcode.js
+++ lesson-10/javascript/weatherAppcode.js
@@ -1,7 +1,7 @@
 var section = document.querySelector('section');
 
 const requestWeather = new XMLHttpRequest();
-requestWeather.open("Get", "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2993fd28d6743adbbe3af79713207ef5", true);
+requestWeather.open("Get", "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=5e326ad81821015729beb090b44212b5", true);
 requestWeather.responeType = "json";
 requestWeather.send();
 requestWeather.onload = function () {
