f7c8f160c13f74c9860786f4326af01339b591b0
Michelle Alley Account <shecalledme@gmail.com>
2019-07-05 19:32:13 -0500

    updates

--- lesson-10/javascript/preston5day.js
+++ lesson-10/javascript/preston5day.js
@@ -1,12 +1,21 @@
-let section = document.querySelector('prestonFive');
-let weatherRequest = new XMLHttpRequest();
-weatherRequest.open("Get", "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2993fd28d6743adbbe3af79713207ef5", true);
-weatherRequest.responeType = "json";
-weatherRequest.send();
+const requestWeather = new XMLHttpRequest();
+requestWeather.open("Get", "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=62a1ef011397ca3b701c6e07cdb3cb67", true);
+requestWeather.responeType = "json";
+requestWeather.send();
+requestWeather.onload = function () {
+   let weatherData = JSON.parse(requestWeather.responseText);
+   console.log(weatherData);
+   
+   document.getElementById('tableTemp') = weatherData.main.temp;
 
-weatherRequest.onload = function () {
-   let weatherFive = JSON.parse(weatherRequest.responseText);
-   console.log(weatherFive);
+   const iconcode = weatherData.weather[0].icon;
+  
 
-    document.getElementById('prestonFive') = weatherFive.weather.description;
+   const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+   console.log(icon_path);
+
+   document.getElementById(weather_icon).src = icon_path;
+
 }
+
+	
