7041dd50d48a3a1bd35e97570d273a53e79fdbd5
Michelle Alley Account <shecalledme@gmail.com>
2019-07-08 14:07:17 -0500

    updates

--- lesson-11/javascript/preston5day.js
+++ lesson-11/javascript/preston5day.js
@@ -1,7 +1,7 @@
 
 
 const requestWeather = new XMLHttpRequest();
-requestWeather.open("Get", "//api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=62a1ef011397ca3b701c6e07cdb3cb67", true);
+requestWeather.open("Get", "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=62a1ef011397ca3b701c6e07cdb3cb67", true);
 requestWeather.responeType = "json";
 requestWeather.send();
 
