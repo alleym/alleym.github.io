20adc07c6c764d76795463065faabb1bc89f35dd
Michelle Alley Account <shecalledme@gmail.com>
2019-07-08 13:58:56 -0500

    trying to fix java api

--- lesson-11/javascript/preston5day.js
+++ lesson-11/javascript/preston5day.js
@@ -1,21 +1,21 @@
 
 
 const requestWeather = new XMLHttpRequest();
-requestWeather.open("Get", "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=62a1ef011397ca3b701c6e07cdb3cb67", true);
+requestWeather.open("Get", "//api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=62a1ef011397ca3b701c6e07cdb3cb67", true);
 requestWeather.responeType = "json";
 requestWeather.send();
 
 requestWeather.onload = function () {
    let weatherData = JSON.parse(requestWeather.responseText);
- 
+   console.log(weatherData);
 
-   document.getElementById('weatherFive').innerHTML = weatherData.list[0].main.temp;
+      document.getElementById('weatherFive').innerHTML = weatherData.list[0].main.temp;
    document.getElementById('weatherFive1').innerHTML = weatherData.list[1].main.temp;
    document.getElementById('weatherFive2').innerHTML = weatherData.list[2].main.temp;
    document.getElementById('weatherFive3').innerHTML = weatherData.list[3].main.temp;
    document.getElementById('weatherFive4').innerHTML = weatherData.list[4].main.temp;
 
-   const iconcode = weatherData.list.weather[0].icon;
+   const iconcode = weatherData.list[0].weather[0].icon;
    console.log(iconcode);
    const icon_path = "http://openweathermap.org/img/w" + iconcode +".png";
    document.getElementById('weather_icon').src = icon_path;
