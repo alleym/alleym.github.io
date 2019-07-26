efa7f2bc84098f5f85de60b6009310ce9ff43f63
Michelle Alley Account <shecalledme@gmail.com>
2019-07-26 00:07:22 -0500

    last push

--- final-website/javascript/oaklandinfo.js
+++ final-website/javascript/oaklandinfo.js
@@ -7,5 +7,13 @@ cityINfo.onload = function () {
    console.log(cityData);
 
    document.getElementById("templename").innerHTML = cityData.oakland.name;
+   document.getElementById("address").innerHTML = cityData.oakland.address;
+   document.getElementById("tele").innerHTML = cityData.oakland.telephone;
+   document.getElementById("email").innerHTML = cityData.oakland.email;
+   document.getElementById("temphistory").innerHTML = cityData.oakland.history;
+   document.getElementById("ordinacne").innerHTML = cityData.oakland.ordiance;
+   document.getElementById("session").innerHTML = cityData.oakland.session;
+
+
 
 }
--- final-website/javascript/oaklandweather.js
+++ final-website/javascript/oaklandweather.js
@@ -1,5 +1,5 @@
 const oaklandRequest = new XMLHttpRequest();
-oaklandRequest.open("Get", "//api.openweathermap.org/data/2.5/weather?id=5378538&units=imperial&appid=2e0c6365a9690870166df0060967442b", true);
+oaklandRequest.open("Get", "https://api.openweathermap.org/data/2.5/weather?id=5378538&units=imperial&appid=2e0c6365a9690870166df0060967442b", true);
 oaklandRequest.responeType = "json";
 oaklandRequest.send();
 oaklandRequest.onload = function () {
