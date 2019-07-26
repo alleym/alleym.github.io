b74bba06b4e6039135a83b7919fe293b77501031
Michelle Alley Account <shecalledme@gmail.com>
2019-07-25 23:09:48 -0500

    almost final updates

--- final-website/javascript/washdcweather.js
+++ final-website/javascript/washdcweather.js
@@ -1,5 +1,5 @@
 const weatherRequest = new XMLHttpRequest();
-dcRequest.open("Get", "http://api.openweathermap.org/data/2.5/weather?id=4366164&units=imperial&appid=9e317db8a74c77f096f4971cbe5ef822", true);
+dcRequest.open("Get", "//api.openweathermap.org/data/2.5/weather?id=4366164&units=imperial&appid=9e317db8a74c77f096f4971cbe5ef822", true);
 dcRequest.responeType = "json";
 dcRequest.send();
 dcRequest.onload = function () {
